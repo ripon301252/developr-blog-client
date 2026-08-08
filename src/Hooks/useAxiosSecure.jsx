5b24332b5f44dd1da13e182251b1789c2f0a521f
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-08-08 22:55:47 +0600

    live server update

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { useEffect } from "react";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://developr-blog-server.onrender.com",
 });
 
 const useAxiosSecure = () => {
